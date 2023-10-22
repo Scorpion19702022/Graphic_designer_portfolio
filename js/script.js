1a62d0d36d1c3ea539181e8668ce6a67db2d127c
skorpion <jmosinski@o2.pl>
2023-10-22 19:47:58 +0200

    added js for nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
+const btn = document.querySelector('.burger-btn')
+const navbar = document.querySelector('.navbar')
+const links = document.querySelectorAll('.navbar__link')
+
 const icons = document.querySelectorAll('.header__skillsimage')
 const body = document.querySelector('body')
+
+console.log(btn)
+console.log(navbar)
+console.log(links)
 
 body.onload = () => {
 	loadIcons()
@@ -13,8 +21,13 @@ const loadIcons = () => {
 	}, 800)
 }
 
-// const loadIcons = () => {
-// 	setTimeout(() => {
-// 		icons.class
-// 	}, 800)
-// }
+const handleBtn = () => {
+	navbar.classList.toggle('navbarview')
+	links.forEach(link => {
+		link.addEventListener('click', () => {
+			navbar.classList.remove('navbarview')
+		})
+	})
+}
+
+btn.addEventListener('click', handleBtn)
